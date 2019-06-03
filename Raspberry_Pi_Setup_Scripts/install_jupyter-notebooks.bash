5c6f79bb12f634bdc43975dfe91996b952f530ac
Edgar Garza <45373932+edgargarza7@users.noreply.github.com>
2019-06-03 16:53:12 -0500

    Update install_jupyter-notebooks.bash

--- Raspberry_Pi_Setup_Scripts/install_jupyter-notebooks.bash
+++ Raspberry_Pi_Setup_Scripts/install_jupyter-notebooks.bash
@@ -2,6 +2,7 @@
 echo "This script will install Jupyter Notebooks and Python3 Kernel"
 sudo apt-get install -y python3-dev
 sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install jsonschema
 sudo python3 -m pip install jupyter
 sudo apt-get install -y python-seaborn python-pandas ttf-bitstream-itavera python-matplotlib
 sudo ipython3 kernelspec install-self
