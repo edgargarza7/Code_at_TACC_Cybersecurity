fb96abfcc9daa397b4fbdc082b6a283db397aa6e
Edgar Garza <egarza@tacc.utexas.edu>
2019-06-04 11:14:41 -0500

    Update configure_jupyter-notebooks.bash

--- Raspberry_Pi_Setup_Scripts/configure_jupyter-notebooks.bash
+++ Raspberry_Pi_Setup_Scripts/configure_jupyter-notebooks.bash
@@ -2,6 +2,6 @@
 echo "Configuring Jupyter Notebooks..."
 sudo apt-get install expect -y
 sleep 5
-/usr/local/bin/'jupyter notebook' --generate-config
+/usr/local/bin/jupyter notebook --generate-config
 echo "c.NotebookApp.ip = '*'" >> /home/pi/.jupyter/jupyter_notebook_config.py
 ./jupyter-notebook-passwordset.expect
